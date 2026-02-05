0de9c98b434002067e73181f30dc6db682445d0c
Jeanette Wiktorsson <leiia@hotmail.com>
2026-02-05 10:46:03 +0100

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import "./App.css";
+﻿import "./App.css";
 import data from "./data.json";
 import Book from "./Book";
+import Header from "./Header";
 
 function App() {
   const { books } = data;
 
   return (
-    <>
+    <div className="page">
+      <Header />
       <div className="grid">
         {books.map((book) => (
           <Book key={book.title} book={book} />
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+﻿function Header() {
+  return (
+    <header className="header">
+      <h1 className="header-title">Book Library</h1>
+      <p className="header-subtitle">A calm place to browse timeless stories.</p>
+    </header>
+  );
+}
+
+export default Header;
